262dcf5c6635f1f0b8e5888335b3550df110d66d
jmhs11 <josemariahs@gmail.com>
2021-05-17 22:19:37 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let menuState = false;
+let x = document.getElementById("navigation");
+
+document.getElementById('menu').addEventListener('click', toogleMenu)
+
+function toogleMenu() {
+
+    if (!menuState) {
+        openMenu();
+    } else {
+        closeMenu();
+    }
+}
+
+window.onresize = function () {
+    if (window.innerWidth >= 820) {
+        closeMenu();
+    }
+}
+
+closeMenu();
+
+function closeMenu() {
+    x.style.left = '-160px';
+    menuState = false;
+}
+
+function openMenu() {
+    x.style.left = "0";
+    menuState = true;
+}
